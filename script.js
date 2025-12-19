e3cd88daea80670839daf64777ca3b287ca18ba3
Mateus Silveira <mawwteus12@gmail.com>
2025-12-19 19:01:56 -0300

    foi

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const testeElemento = document.getElementById('teste')
 mensagemElemento.textContent = 'Olá, Mundo! Este é meu primeiro Hello World com JavaScript!';
 
 testeElemento.textContent='modificado nova mensagem '
+
+
+let strNumero1 = "42";
+let strNumero2 = "3.14";
+
+let intNumero = parseInt(strNumero1);
+let floatNumero = parseFloat(strNumero2);
+
+console.log("Número inteiro: " + intNumero); // 42
+console.log("Número de ponto flutuante: " + floatNumero); // 3.14
+
+
+let strNumero = "50";
+let numero = +strNumero;
+
+console.log("Número: " + numero); // 50
