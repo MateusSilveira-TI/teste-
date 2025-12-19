e59773e8020ccd29157737373c7eff29e5fe6813
Mateus Silveira <mawwteus12@gmail.com>
2025-12-19 14:42:21 -0300

    meu primeiro teste

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Seleciona o elemento HTML com o id 'mensagem'
+const mensagemElemento = document.getElementById('mensagem');
+const testeElemento = document.getElementById('teste')
+
+// Modifica o texto do elemento
+mensagemElemento.textContent = 'Olá, Mundo! Este é meu primeiro Hello World com JavaScript!';
+
+testeElemento.textContent='modificado nova mensagem '
